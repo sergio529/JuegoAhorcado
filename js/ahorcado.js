10104b44df9b63830e6260e6bc9966ae5fec460a
Sergio <sergiosolarte529@hotmail.com>
2022-06-06 12:30:04 -0500

    commit inicial

---
+++ js/ahorcado.js
@@ -0,0 +1,38 @@
+var words=["ALURA", "AHORCADO", "ORACLE","XD"];
+var board= document.getElementById("draw").getContext("2D");
+console.log(board);
+var lettersUsed = [];
+var gameWord= "";
+var lives=9;
+
+function chooseWord(){
+ var  wordChoosen= words[Math.floor(Math.random()*words.length)];
+ console.log(wordChoosen);
+ return wordChoosen;;
+
+}
+
+function drawLines(){
+
+    board.linewidth=6;
+    board.lineCap="round";
+    board.lineJoin="round";
+    board.strokeStyle= "#FFFFFF";
+    
+    board.beginPath()
+    
+
+    
+    var spaceInLines=600/gameWord.length;
+    for (let i=0 ; i<gameWord.length;i++){
+        board.moveTo(500+(spaceInLines*i),640)
+        board.lineTo(550+(spaceInLines*i),640)
+    }
+    board.stroke();
+    board.closePath();
+     
+}
+
+drawLines(chooseWord);
+
+
---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+function changePage(site){
+    window.location.href=site;
+    console.log(site)
+}
